25ddb75beb14f2552131811b007f3c73007c0d8f
rohit nagappa <rnagappa35@gmail.com>
2020-09-24 07:57:41 +0530

    added entity

---
+++ src/main/java/com/demo/jpa/hibernate/Spring_JPA_Hibernate/entity/Course.java
@@ -0,0 +1,45 @@
+package com.demo.jpa.hibernate.Spring_JPA_Hibernate.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+/**
+ * Without entity it is like any other java bean
+ * @author 91783
+ *
+ */
+@Entity
+public class Course {
+	
+	@Id
+	@GeneratedValue
+	private long id;
+	
+	private String name;
+	
+	public Course() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+	public void setId(long id) {
+		this.id = id;
+	}
+
+
+	public Course(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+	
+}

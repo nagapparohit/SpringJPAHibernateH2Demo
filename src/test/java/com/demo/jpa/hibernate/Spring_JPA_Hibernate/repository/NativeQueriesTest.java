494d3fc54c3269daca5f6f66ded30b80773fee05
rohit nagappa <rnagappa35@gmail.com>
2020-09-30 00:10:12 +0530

    added mass update using native sql as with jpa this is not possbibe

--- src/test/java/com/demo/jpa/hibernate/Spring_JPA_Hibernate/repository/NativeQueriesTest.java
+++ src/test/java/com/demo/jpa/hibernate/Spring_JPA_Hibernate/repository/NativeQueriesTest.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.Query;
+import javax.transaction.Transactional;
+
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
@@ -47,8 +49,21 @@ public class NativeQueriesTest {
 	void native_queries_with_named_parameter() {
 		 Query query = em.createNativeQuery("Select * from Course where id = :id",Course.class);
 		 query.setParameter("id", 10001L);
-		 List resultList = query.getResultList();
+		 @SuppressWarnings("rawtypes")
+		List resultList = query.getResultList();
 		 logger.info("\nNative  basic\nSelect * from Course where id = :id --> \n{} ",resultList);
+	}
+	
+	/**
+	 * for mass update always use native queris as mass update is not supported in jpa if you do by jps you need to read and update 
+	 * each row from db
+	 */
+	@Test
+	@Transactional
+	void native_queries_with_update() {
+		 Query query = em.createNativeQuery("update Course set last_updated_date=sysdate()");
+		 int no_rows_updated = query.executeUpdate();
+		 logger.info("\nno of rows updated --->{} ",no_rows_updated);
 	}
 	
 }

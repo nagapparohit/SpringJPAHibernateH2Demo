7fba0773fa24e74af5dc7912838149b51a8bd331
rohit nagappa <rnagappa35@gmail.com>
2020-10-01 08:34:22 +0530

    Eager fetch test in ont to one realtion

---
+++ src/test/java/com/demo/jpa/hibernate/Spring_JPA_Hibernate/repository/StudentRepositoryTest.java
@@ -0,0 +1,36 @@
+package com.demo.jpa.hibernate.Spring_JPA_Hibernate.repository;
+
+import javax.persistence.EntityManager;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.demo.jpa.hibernate.Spring_JPA_Hibernate.SpringJpaHibernateApplication;
+import com.demo.jpa.hibernate.Spring_JPA_Hibernate.entity.Student;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = SpringJpaHibernateApplication.class)
+public class StudentRepositoryTest {
+
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
+	
+	@Autowired
+	StudentRepository repository;
+	
+	@Autowired
+	EntityManager em;
+	
+	@Test
+	void retrieveStuentAndPassportDetails() {
+		Student student = em.find(Student.class, 20001L);
+		logger.info("student --> {}",student);
+		logger.info("student passport detials --> {}",student.getPassport());		
+	}
+		
+}
